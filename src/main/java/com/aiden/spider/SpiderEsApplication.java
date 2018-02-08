1325191f53724240e2ecb29044bc49ebea6c25b6
tim_yu <yutingshan@bbdservice.com>
2018-02-08 14:37:44 +0800

    init

---
+++ src/main/java/com/aiden/spider/SpiderEsApplication.java
@@ -0,0 +1,12 @@
+package com.aiden.spider;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpiderEsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpiderEsApplication.class, args);
+    }
+}
